c3d0bb3ef9f8d93ade403eaf85c17589d9bd33ec
Will Stallwood <will@cipherprime.com>
2016-11-03 00:22:25 -0400

    Bsic Sine Movement to test Fade Renderer

--- js/main.js
+++ js/main.js
@@ -12,13 +12,26 @@ function main()
 		testing.graphics.beginFill("DeepSkyBlue").rect(0,0,50,50);
 		testing.x = window.innerWidth * 0.5;
 		testing.y = window.innerHeight * 0.5;
+		testing.originX = testing.x;
+		testing.counter = 0;
+		testing.increment = .1;
+		testing.amplitude = 50;
+		testing.addEventListener( "tick" , testingTick );
 	stage.addChild(testing);  
 
 	// Extension Test
   	var extend_test = new ExtendedContainer();
 		extend_test.output();
 }
-	
+
+var counter = 0;
+
+function testingTick( event )
+{
+	event.target.x = event.target.originX + Math.sin( event.target.counter ) * event.target.amplitude;
+	event.target.counter += event.target.increment;
+}	
+
 function keyPressed( event )
 {
 	//Keycodes found at http://keycode.info
